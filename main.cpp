c18065121ab441b52def1d0d0948a96a0685cab1
peplxx <seregaopkymelnikov@yandex.ru>
2024-10-30 20:49:34 +0300

    feat: add inverse matrix computation

--- main.cpp
+++ main.cpp
@@ -3,55 +3,52 @@
 #include <vector>
 #include <numeric>
 #include <iomanip>
+#include <stdexcept>
 
 std::string buffer;
-typedef double element_t;
-typedef std::vector<element_t> row_t;
+typedef std::vector<double> row_t;
 typedef std::vector<row_t> matrix_t;
-std::vector<element_t> vbuffer;
-
-
+std::vector<double> vbuffer;
 
 template<typename T>
-std::stringstream& operator >> (std::stringstream& ss, std::vector<T>& buffer){
+std::stringstream& operator>>(std::stringstream& ss, std::vector<T>& buffer) {
     buffer.clear();
-    for (T temp;ss >> temp;) buffer.push_back(temp);
+    for (T temp; ss >> temp;) buffer.push_back(temp);
     return ss;
 }
+
 template<typename T>
-std::ostream& operator << (std::ostream& os, const std::vector<T>& v){
-    for (T e:v)os << e << ' ';
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    for (T e : v) os << e << ' ';
     return os;
 }
 
 template<typename T>
-std::ostream& operator << (std::ostream& os, const std::vector<std::vector<T>>& v){
-    for (auto e:v)os << e << '\n';
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& v) {
+    for (auto e : v) os << e << '\n';
     return os;
 }
 
-
-inline element_t operator * (const row_t& a,const row_t& b) {
+inline double operator*(const row_t& a, const row_t& b) { // dot product
     double acum = 0;
-    for (int i=0; i< a.size(); i++){
-        acum += a[i] * b[i];}
-    return acum;}
+    for (int i = 0; i < a.size(); i++)acum += a[i] * b[i];
+    return acum;
+}
 
-class Matrix{
+class Matrix {
 public:
-    
-    size_t n,m;
+    size_t n, m;
     matrix_t data;
 
-    Matrix(){}
-    Matrix(matrix_t data):data(data){
+    Matrix() {}
+    Matrix(matrix_t data) : data(data) {
         n = data.size();
         m = data[0].size();
     }
 
-    static Matrix input(std::istream& istream){
+    static Matrix input(std::istream& istream = std::cin) {
         matrix_t temp = {};
-        for (getline(istream, buffer);!buffer.empty();getline(istream, buffer)) {
+        for (getline(istream, buffer); !buffer.empty(); getline(istream, buffer)) {
             std::stringstream sstream(buffer);
             sstream >> vbuffer;
             temp.push_back(vbuffer);
@@ -59,71 +56,113 @@ public:
         return Matrix(temp);
     }
 
-    static Matrix diag(Matrix&  x){
+    static Matrix diag(Matrix& x) {
         // Assume that x.n == 1
-        matrix_t temp(x.m, row_t (x.m, 0));
-        for (int i=0;i<x.m;i++)
+        matrix_t temp(x.m, row_t(x.m, 0));
+        for (int i = 0; i < x.m; i++)
             temp[i][i] = x.data[0][i];
         return Matrix(temp);
     }
-    static Matrix identity(size_t size){
-        Matrix ones_vector({row_t (size, 1)});
+
+    static Matrix identity(size_t size) {
+        Matrix ones_vector({ row_t(size, 1) });
         return diag(ones_vector);
     }
-    Matrix T(){
+
+    Matrix T() {
         matrix_t transposed(m);
-        for (int i =0; i< m;i++){transposed[i] = this->operator()(i);}
+        for (int i = 0; i < m; i++) { transposed[i] = this->operator()(i); }
         return Matrix(transposed);
     }
-    row_t operator[] (int t)const {return data[t];} // get row operator
-    row_t operator() (int t)const {                 // get column operator
-        row_t temp(n,0);
-        for (int i = 0; i< n; i++)
+
+    Matrix Inverse() {
+        if (n != m) throw std::invalid_argument("Matrix must be square to calculate inverse");
+
+        Matrix augmented = *this;  // Copy the original matrix
+        Matrix identity_matrix = Matrix::identity(n);
+
+        for (size_t i = 0; i < n; i++)
+            augmented.data[i].insert(augmented.data[i].end(), identity_matrix.data[i].begin(), identity_matrix.data[i].end());
+
+        for (size_t i = 0; i < n; i++) {
+            double pivot = augmented.data[i][i];
+            if (pivot == 0) throw std::runtime_error("Matrix is singular and cannot be inverted");
+
+            for (size_t j = 0; j < 2 * n; j++)
+                augmented.data[i][j] /= pivot;
+
+            for (size_t k = 0; k < n; k++) {
+                if (k != i) {
+                    double factor = augmented.data[k][i];
+                    for (size_t j = 0; j < 2 * n; j++)
+                        augmented.data[k][j] -= factor * augmented.data[i][j];
+                }
+            }
+        }
+
+        matrix_t inverse_data(n, row_t(n));
+        for (size_t i = 0; i < n; i++) {
+            for (size_t j = 0; j < n; j++) {
+                inverse_data[i][j] = augmented.data[i][j + n];
+            }
+        }
+
+        return Matrix(inverse_data);
+    }
+
+    row_t operator[](int t) const { return data[t]; } // Get row operator
+    row_t operator()(int t) const {                   // Get column operator
+        row_t temp(n, 0);
+        for (int i = 0; i < n; i++)
             temp[i] = data[i][t];
         return temp;
-    } 
+    }
 };
-std::ostream& operator << (std::ostream& os, Matrix& m){return os << m.data;}
-std::istream& operator >> (std::istream& is, Matrix& m){ m = Matrix::input(is);}
-Matrix operator * (const Matrix& a, const Matrix& b){ // Matrix multiplication
+
+std::ostream& operator<<(std::ostream& os, Matrix& m) { return os << m.data; }
+std::istream& operator>>(std::istream& is, Matrix& m) { m = Matrix::input(is); }
+Matrix operator*(const Matrix& a, const Matrix& b) { // Matrix multiplication
     // Assume that a.m == b.n
-    matrix_t result(a.n, row_t(b.m,0));
-    for (int i = 0;i < result.size();i++)
-        for (int j = 0; j< result[0].size(); j++){
+    matrix_t result(a.n, row_t(b.m));
+    for (int i = 0; i < result.size(); i++)
+        for (int j = 0; j < result[0].size(); j++) {
             result[i][j] = a[i] * b(j);
-            // std::cout << a[i] << " * " << b(i)  << " = " << result[i][j] << '\n';
+        }
+    return Matrix(result);
+}
+Matrix operator -(const Matrix& a, const Matrix& b) { // Matrix multiplication
+    // Assume that a.m == b.m && a.n == b.n
+    matrix_t result(a.n, row_t(a.m));
+    for (int i = 0; i < result.size(); i++)
+        for (int j = 0; j < result[0].size(); j++) {
+            result[i][j] = a.data[i][j] - b.data[i][j];
         }
     return Matrix(result);
 }
 
-using std::cin,std::cout;
-// By default all vectors are horizontal
-int main(){
-    // cout.precision(1);
+using std::cin, std::cout;
+
+double min(Matrix& m){
+    double ans = 
+}
+
+int main() {
     std::ios::sync_with_stdio(false);
-    Matrix C, A, x, b;
-    
-    double eps = 0.001, alph1 = 0.5, aplph2 = 0.9;
-    cin >> C >> A >> x;
-    // cin >> b;
-    while (true)
-    {
+    Matrix C = Matrix::input(), A = Matrix:: input() , x = Matrix::input(), b;
+    double eps = 0.001, alph1 = 0.5, alph2 = 0.9;
+    while (true) {
         Matrix D = Matrix::diag(x);
-        Matrix _A = A * D;
-        Matrix _C = D * C.T();
+        Matrix A1 = A * D;
         Matrix I = Matrix::identity(x.m);
-        cout << D << '\n';
-        cout << _A << '\n';
-        cout << C << '\n';
-        cout << _C << '\n';
+        Matrix P = I - A1.T() * (A1 * A1.T()).Inverse() * A1;
+        Matrix cp = P *(D * C.T());
+        cout << cp << '\n';
 
         break;
     }
-    
 
     return 0;
 }
-
 /*
 1 1 0 0
 
